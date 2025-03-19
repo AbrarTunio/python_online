777cacfe0ef56f875b47867cc66236f849d0cad6
AbrarTunio <abrar.tunio@yahoo.com>
2025-03-20 01:58:53 +0500

    lecture 2

---
+++ 20250319/variable.py
@@ -0,0 +1,41 @@
+# name = 'CodePro'
+# num1 = 15
+# volume = 12.0
+# boolean = False
+
+
+# print( "My Fav number is :" , num1 )
+# print( f"My fav number is : {num1}" )
+# print("My favourite number is: {}".format(num1)  )
+
+# name  =  "Abrar Hussain"
+# age   = 22
+# fav_color  =  "green"
+
+# print( f"My Name is {name} and I am {age} years old and my fav color is {fav_color}" )
+
+# myName = "CodePro"
+# MyName = "Abrar Hussain"
+# myName = "Unknown"
+
+# print( myName )
+
+# myName = "Abrar Hussain"
+# surname = "Tunio"
+
+# myName , surname      =    surname , myName
+
+# print( myName )
+# print( surname )
+
+
+myName = "Abrar Hussain"
+surname = "Tunio"
+
+fullName = myName + " " + surname
+print(fullName)
+
+
+
+
+
