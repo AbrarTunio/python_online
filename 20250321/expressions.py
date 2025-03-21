ef89728b756a96a252dfa20f26492ac4588fdec3
AbrarTunio <abrar.tunio@yahoo.com>
2025-03-21 22:37:39 +0500

    lecture 3 about expressions

---
+++ 20250321/expressions.py
@@ -0,0 +1,49 @@
+# name = "Abrar Hussain"
+# num = 15
+# myWeight = 70.5
+# habit = True
+
+
+# print(  type( habit )    )
+
+
+#######################
+# EXAMPLE TWO
+######################
+# num1 = "15"
+# num2 = 7
+
+# num1_i = int( num1 )
+
+# # result  =  int( num1 )  + num2
+# result  =  num1_i   + num2
+
+# print(result)
+
+#######################
+# EXAMPLE THREE
+######################
+
+# a = input( "Enter your first Name" )
+# b = input( "Enter your last name" )
+
+result = 10 + 5 * 2 - 3
+# print( result )
+
+# Brackets
+# Exponents
+# Division/Multiplication
+# Plus / Minus
+
+# power = 2 ** 3   # 2*2*2
+
+# print( power )
+
+
+# modulus = 21  //   5
+# print( modulus )
+
+print( 2 != 8 )
+
+
+
---
+++ 20250321/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+num1 = 15
+num2 = 7
+
+result  =  num1 + num2
+
+print(result)
